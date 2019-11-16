beba4d59443e944f8f9f6c8d0749496240e54c2c
SRaccoon <gmande11@naver.com>
2019-11-17 02:49:36 +0900

    index

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('login');
 });
 
+router.get('/seat', function(req, res, next) {
+  res.render('seat');
+});
+
+router.get('/intro', function(req, res, next) {
+  res.render('intro');
+});
+
+router.get('/payment', function(req, res, next) {
+  res.render('seat');
+});
+
+router.get('/reserve', function(req, res, next) {
+  res.render('reserve');
+});
+
 module.exports = router;
